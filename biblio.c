18eba47396191e51996ce8d70aba964a021da32c
Ulitra <martin.bonnafous@ig2i.centralelille.fr>
2025-02-26 08:35:44 +0100

    enfin les bons fichiers

--- biblio.c
+++ biblio.c
@@ -1,13 +1,4 @@
 # include "biblio.h"
-
-void init (T_Bibliotheque *ptrB)
-{
-
-ptrB->nbLivres=0;
-// ou (*ptrB).nbLivres=0;
-}
-
-
 int menu()
 {
 	int choix;
@@ -77,100 +68,11 @@ switch(chx)
 }while(chx!=0);
 
 
-
-
-
-
 return 0;
 
 }
 
-void sauvegarde(T_Bibliotheque *ptrB)
-{
-FILE *fic=NULL; //le type FILE
-int i;
-fic=fopen("baseLivres","w"); // w = le mode = write avec ECRASEMENT
-//fopen renvoie NULL si probleme (disque plein, disque non accessible ...
-if (fic!=NULL)
-	{
-	for(i=0;i<ptrB->nbLivres;i++)
-		{
-//fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
-    fwrite(  &(ptrB->etagere[i])        ,sizeof(T_livre),1,fic);
-
-		}
-	fclose(fic);
-	puts("SAUVEGARDE REUSSIE ..............");
 
 
-
-	}
-	else puts("ECHEC DE SAUVEGARDE  !!!!!  ");
-
-
-
-
-}
-
-
-
-void chargement(T_Bibliotheque *ptrB)
-{
-FILE *fic=NULL; //le type FILE
-int i=0;
-fic=fopen("baseLivres","r"); // r = le mode read
-//fopen renvoie NULL si probleme (disque plein, disque non accessible ...
-if (fic!=NULL)
-	{
-	do
-		{
-
-		fread(  &(ptrB->etagere[i]) ,sizeof(T_livre),1,fic);
-		i++;
-		}
-		while(!feof(fic));
-	fclose(fic);
-	ptrB->nbLivres=i-1;
-	puts("CHARGEMENT  REUSSI ..............");
-	}
-	else puts("ECHEC DE CHARGEMENT  !!!!!  ");
-
-}
-
-
-
-int ajouterLivre(T_Bibliotheque  *ptrB)
-{
-if (ptrB->nbLivres<CAPACITE_BIBLIO)// reste t il de la place?
-	{
-	saisirLivre(&(ptrB->etagere[ptrB->nbLivres]));
-	ptrB->nbLivres++;
-	return 1;
-	}
-	
-	
-return 0;
-	
-}
-
-
-int  afficherBibliotheque(const T_Bibliotheque  *ptrB)
-{
-int i;
-if(ptrB->nbLivres==0)
-	return 0;
-	else
-		{
-		for(i=0;i<ptrB->nbLivres;i++)
-			{
-			afficherLivre( &(ptrB->etagere[i])  );
-			
-			}
-
-
-		
-		return 1;
-		}
-}
 
 
